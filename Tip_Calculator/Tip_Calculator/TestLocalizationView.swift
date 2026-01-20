3d397d4bb7ce09d23549c30091388a17666ed3e2
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-20 10:04:36 -0500

    add TestLocalizationView to test localization

---
+++ Tip_Calculator/Tip_Calculator/TestLocalizationView.swift
@@ -0,0 +1,26 @@
+//
+//  TestLocalizationView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 1/20/26.
+//
+
+import SwiftUI
+
+struct TestLocalizationView: View {
+    var body: some View {
+        Text("Amount")
+        Text("Bill")
+        Text("Choose Tip")
+        Text("Custom")
+        Text("Enter Total")
+        Text("Split")
+        Text("Summary")
+        Text("Tip Calculator")
+        Text("Total per person")
+    }
+}
+
+#Preview {
+    TestLocalizationView()
+}
