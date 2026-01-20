a6baee217f1f3d5d0610e3fb0c95b537d972cc95
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-20 09:30:27 -0500

    Tip_Calculator intial commit

---
+++ Tip_Calculator/Tip_Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 1/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tip_Calculator/Tip_Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
