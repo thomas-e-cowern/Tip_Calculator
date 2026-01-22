d4038e5aab356636bd3cf64fcf108916c11629db
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-22 08:23:02 -0500

    add BackgrouneView and SineView to project

---
+++ Tip_Calculator/Tip_Calculator/Views/BackgroundView/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 1/22/26.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
---
+++ Tip_Calculator/Tip_Calculator/Views/BackgroundView/SineView.swift
@@ -0,0 +1,18 @@
+//
+//  SineView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 1/22/26.
+//
+
+import SwiftUI
+
+struct SineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SineView()
+}
