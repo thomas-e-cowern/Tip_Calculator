d6f7d8160387e41e51b3a60ef9d296d3d057770b
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-23 13:27:11 -0500

    add TestViews folder, move TestLocalizationView to TestViews folder

---
+++ Tip_Calculator/Tip_Calculator/TestViews/TestColorsView.swift
@@ -0,0 +1,18 @@
+//
+//  TestColorsView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 1/23/26.
+//
+
+import SwiftUI
+
+struct TestColorsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestColorsView()
+}
