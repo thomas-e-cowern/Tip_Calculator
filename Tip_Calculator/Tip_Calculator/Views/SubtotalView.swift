76e3ba30feb6e14c14d3df718f9aafb35f751c2f
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-05 08:17:31 -0500

    add SubtotalView to project

---
+++ Tip_Calculator/Tip_Calculator/Views/SubtotalView.swift
@@ -0,0 +1,18 @@
+//
+//  SubtotalView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 2/5/26.
+//
+
+import SwiftUI
+
+struct SubtotalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubtotalView()
+}
