0473419e1fcf73c807f53b8ef0a0e964b6167cad
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-02-09 08:38:34 -0500

    add PaymentSummaryView to project

---
+++ Tip_Calculator/Tip_Calculator/Views/PaymentSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentSummaryView.swift
+//  Tip_Calculator
+//
+//  Created by Thomas Cowern on 2/9/26.
+//
+
+import SwiftUI
+
+struct PaymentSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentSummaryView()
+}
